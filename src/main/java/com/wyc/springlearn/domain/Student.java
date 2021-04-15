e99e8f96f5e64a6e005a4a3724389f2cf661106e
za-wangyiche <yiche.wang@zatech.com>
2021-04-15 16:39:58 +0800

    add swagger

--- src/main/java/com/wyc/springlearn/domain/Student.java
+++ src/main/java/com/wyc/springlearn/domain/Student.java
@@ -1,12 +1,20 @@
 package com.wyc.springlearn.domain;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
+@ApiModel(description="学生实体")
 public class Student {
 
+    @ApiModelProperty("编号")
     private Long id;
+
+    @ApiModelProperty("姓名")
     private String name;
+
+    @ApiModelProperty("年龄")
     private Integer age;
 
 }
--- src/main/java/com/wyc/springlearn/web/StudentController.java
+++ src/main/java/com/wyc/springlearn/web/StudentController.java
@@ -1,33 +1,40 @@
 package com.wyc.springlearn.web;
 
 import com.wyc.springlearn.domain.Student;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
 @RestController
+@Api(tags = "学生管理")
 @RequestMapping(value = "/students")
 public class StudentController {
 
     static Map<Long, Student> students = Collections.synchronizedMap(new HashMap<Long, Student>());
 
+    @ApiOperation("获取学生列表")
     @GetMapping("/")
     public List<Student> getStudentList() {
         List<Student> r = new ArrayList<Student>(students.values());
         return r;
     }
 
+    @ApiOperation("创建学生")
     @PostMapping("/")
     public String postStudent(@RequestBody Student student) {
         students.put(student.getId(), student);
         return "success";
     }
 
+    @ApiOperation("获取学生信息")
     @GetMapping("/{id}")
     public Student getStudent(@PathVariable Long id) {
         return students.get(id);
     }
 
+    @ApiOperation("修改学生信息")
     @PutMapping("/{id}")
     public String putStudent(@PathVariable Long id, @RequestBody Student student) {
         Student s = students.get(id);
@@ -37,6 +44,7 @@ public class StudentController {
         return "success";
     }
 
+    @ApiOperation("删除学生")
     @DeleteMapping("/{id}")
     public String deleteStudent(@PathVariable Long id) {
         students.remove(id);
