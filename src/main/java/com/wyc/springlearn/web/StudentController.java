6599be5d434ac8a3c16e34de86d76d6eda715b62
za-wangyiche <yiche.wang@zatech.com>
2021-05-27 16:29:50 +0800

    add mybatis

--- src/main/java/com/wyc/springlearn/web/StudentController.java
+++ src/main/java/com/wyc/springlearn/web/StudentController.java
@@ -1,54 +1,50 @@
 package com.wyc.springlearn.web;
 
+import com.wyc.springlearn.dao.StudentDao;
 import com.wyc.springlearn.domain.Student;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.*;
+import java.util.List;
 
 @RestController
 @Api(tags = "学生管理")
 @RequestMapping(value = "/students")
 public class StudentController {
 
-    static Map<Long, Student> students = Collections.synchronizedMap(new HashMap<Long, Student>());
+    @Autowired
+    private StudentDao studentDao;
 
     @ApiOperation("获取学生列表")
     @GetMapping("/")
     public List<Student> getStudentList() {
-        List<Student> r = new ArrayList<Student>(students.values());
-        return r;
+        return studentDao.getAllStudents();
     }
 
     @ApiOperation("创建学生")
     @PostMapping("/")
-    public String postStudent(@RequestBody Student student) {
-        students.put(student.getId(), student);
-        return "success";
+    public Integer postStudent(@RequestBody Student student) {
+        return studentDao.insert(student);
     }
 
     @ApiOperation("获取学生信息")
     @GetMapping("/{id}")
-    public Student getStudent(@PathVariable Long id) {
-        return students.get(id);
+    public Student getStudent(@PathVariable Integer id) {
+        return studentDao.getById(id);
     }
 
     @ApiOperation("修改学生信息")
     @PutMapping("/{id}")
-    public String putStudent(@PathVariable Long id, @RequestBody Student student) {
-        Student s = students.get(id);
-        s.setName(student.getName());
-        s.setAge(student.getAge());
-        students.put(id, s);
-        return "success";
+    public Integer putStudent(@PathVariable Integer id, @RequestBody Student student) {
+        return studentDao.updateById(id, student);
     }
 
     @ApiOperation("删除学生")
     @DeleteMapping("/{id}")
-    public String deleteStudent(@PathVariable Long id) {
-        students.remove(id);
-        return "success";
+    public Integer deleteStudent(@PathVariable Integer id) {
+        return studentDao.deleteById(id);
     }
 
 }
